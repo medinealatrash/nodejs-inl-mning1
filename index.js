5134c6b4d9c4c52b39284c53fa06ef770296c978
Medine <you@example.com>
2021-04-28 18:20:51 +0200

    made some changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/:id/:on', (req, res) =>{
      let file;
      let property= {};
 
-     let onOff= req.params.on === "on" ? (id === "LIG1" ? (color = "#800080",  brightness= 0.7, state= "on" ): id=== "LIG2" ? (color = "0000FF", brightness= 1,state= "on" ): id=== "LIG3" ? (color = "#00FFFF",  brightness= 0.3,state= "on"  ) :  id ===  "LOC1" ? locked= true :  id ===  "VAC1" ? (on = true, state= "charging"  ):  id==="BLI1" ?(on = true, state= "down" ) :  id=== "AC1"?(on = true, state= "on" ) : id=== "CAM1" ? (on = true) : id=== "SPE1" ? (on = true, file= "testfile.ogg"):     false         ) 
+     let onOff= req.params.on === "on" ? (id === "LIG1" ? (color = "#800080",  brightness= 0.7, state= "on",on = true ): id=== "LIG2" ? (color = "0000FF", brightness= 1,state= "on",on = true ): id=== "LIG3" ? (color = "#00FFFF",  brightness= 0.3,state= "on",on = true  ) :  id ===  "LOC1" ? locked= true :  id ===  "VAC1" ? (on = true, state= "charging"  ):  id==="BLI1" ?(on = true, state= "down" ) :  id=== "AC1"?(on = true, state= "on" ) : id=== "CAM1" ? (on = true) : id=== "SPE1" ? (on = true, file= "testfile.ogg"):     false         ) 
      
      : id === "LIG1" ? (state= "off" ): id=== "LIG2" ? (state= "off" ): id=== "LIG3" ? (state= "off"  ) :  id ===  "LOC1" ? locked= false :  id ===  "VAC1" ? (on = false, state= "off"  ):  id==="BLI1" ?(on = false, state= "up" ) :  id=== "AC1"?(on = false, state= "off" ) : id=== "CAM1" ? (on = false) : id=== "SPE1" ? (on = false, file= "testfile.mp3") : (onOff = "on")
  
