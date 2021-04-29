16b60cc748a8375a60f785c31de01f4afdb247d4
Medine <you@example.com>
2021-04-29 11:47:43 +0200

    finish

--- index.js
+++ index.js
@@ -5,26 +5,90 @@ app.listen(3000, () => {
     console.log('API for smart home 1.1 up n running.')
 })
 
-
-app.get('/:id/:on', (req, res) =>{
-    let id= req.params.id;
-
-    let color;
-     let brightness;
-     let locked;
+// skriva http://localhost:3000/camera/VAC1/on för att starta kameran
+// skriva http://localhost:3000/camera/VAC1/(valfri text) för att stänga av kameran
+ app.get('/camera/:id/:on', (req, res) =>{
+     let id= req.params.id;
      let state;
      let on; 
-     let file;
-     let property= {};
-
-     let onOff= req.params.on === "on" ? (id === "LIG1" ? (color = "#800080",  brightness= 0.7, state= "on",on = true ): id=== "LIG2" ? (color = "0000FF", brightness= 1,state= "on",on = true ): id=== "LIG3" ? (color = "#00FFFF",  brightness= 0.3,state= "on",on = true  ) :  id ===  "LOC1" ? locked= true :  id ===  "VAC1" ? (on = true, state= "charging"  ):  id==="BLI1" ?(on = true, state= "down" ) :  id=== "AC1"?(on = true, state= "on" ) : id=== "CAM1" ? (on = true) : id=== "SPE1" ? (on = true, file= "testfile.ogg"):     false         ) 
-     
-     : id === "LIG1" ? (state= "off" ): id=== "LIG2" ? (state= "off" ): id=== "LIG3" ? (state= "off"  ) :  id ===  "LOC1" ? locked= false :  id ===  "VAC1" ? (on = false, state= "off"  ):  id==="BLI1" ?(on = false, state= "up" ) :  id=== "AC1"?(on = false, state= "off" ) : id=== "CAM1" ? (on = false) : id=== "SPE1" ? (on = false, file= "testfile.mp3") : (onOff = "on")
- 
     
+     let onOff= req.params.on === "on" ?  (on = true, state= "on" ) : (on = false, state= "off" )
  
+     db.get('devices')
+     .find({id:id})
+     .assign({on:on,  state:state})
+     .value();
     
-     db.get('devices').find({id:id}).assign({on : on, brightness :brightness, color :color, locked : locked, state:state, file:file}).value();
+     update();
+     res.send({ msg: `device  is now ${onOff}` });
+ }) 
+// skriva http://localhost:3000/light/(lampas id)/on för att tända specifika lampan( skriv specifik id)
+//skriva http://localhost:3000/light/(lampas id)/(valfri text) för att släcka specifika lampan( skriv specifik id)
+ app.get('/light/:id/:on', (req, res) =>{
+    let id= req.params.id;
+    
+     let onOff= req.params.on === "on" ? true : false
+     db.get('devices')
+     .find({id: id})
+     .assign({on : onOff, brightness: 1})
+     .value();
+    
      update();
      res.send({ msg: `device ${id} is now ${onOff}` });
- })
+ }) 
+// skriva http://localhost:3000/vacuum/VAC1/on för att starta dammsugaren
+// skriva http://localhost:3000/vacuum/VAC1/(valfri text) för att stopa dammsugaren
+ app.get('/vacuum/:id/:on', (req, res) =>{
+    let id= req.params.id;
+
+     let onOff= req.params.on === "on" ? true : false
+     db.get('devices')
+     .find({id: id})
+     .assign({on : onOff})
+     .value();
+    
+     update();
+     res.send({ msg: `device ${id} is now ${onOff}` });
+ }) 
+// skriva http://localhost:3000/speaker/SPE1/on för att starta högtalaren
+// skriva http://localhost:3000/speaker/SPE1/(valfri text)  för att stänga av högtalaren
+ app.get('/speaker/:id/:on', (req, res) =>{
+    let id= req.params.id;
+
+     let onOff= req.params.on === "on" ? true : false
+     db.get('devices')
+     .find({id: id})
+     .assign({on : onOff})
+     .value();
+    
+     update();
+     res.send({ msg: `device ${id} is now ${onOff}` });
+ }) 
+// skriva http://localhost:3000/blind/BLI1/on för att rulla ner rullgardin
+// skriva http://localhost:3000/blind/BLI1/(valfri text) för att rulla upp rullgardin
+ app.get('/blind/:id/:on', (req, res) =>{
+    let id= req.params.id;
+
+     let onOff= req.params.on === "on" ? true : false
+     db.get('devices')
+     .find({id: id})
+     .assign({on : onOff})
+     .value();
+    
+     update();
+     res.send({ msg: `device ${id} is now ${onOff}` });
+ }) 
+ // skriva http://localhost:3000/lock/LOC1/on för att öppna dörr låset
+  // skriva http://localhost:3000/lock/LOC1/(valfri text)  för att stänga dörr låset
+ app.get('/lock/:id/:on', (req, res) =>{
+    let id= req.params.id;
+
+     let onOff= req.params.on === "on" ? true : false
+     db.get('devices')
+     .find({id: id})
+     .assign({on : onOff, locked: onOff})
+     .value();
+    
+     update();
+     res.send({ msg: `device ${id} is now ${onOff}` });
+ }) 
