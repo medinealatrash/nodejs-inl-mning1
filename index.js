7d1c20e49ffdf83aa4e86566efb3d4b23f129f2e
Medine <you@example.com>
2021-04-29 13:17:09 +0200

    corrected

--- index.js
+++ index.js
@@ -5,23 +5,7 @@ app.listen(3000, () => {
     console.log('API for smart home 1.1 up n running.')
 })
 
-// skriva http://localhost:3000/camera/VAC1/on för att starta kameran
-// skriva http://localhost:3000/camera/VAC1/(valfri text) för att stänga av kameran
- app.get('/camera/:id/:on', (req, res) =>{
-     let id= req.params.id;
-     let state;
-     let on; 
-    
-     let onOff= req.params.on === "on" ?  (on = true, state= "on" ) : (on = false, state= "off" )
- 
-     db.get('devices')
-     .find({id:id})
-     .assign({on:on,  state:state})
-     .value();
-    
-     update();
-     res.send({ msg: `device  is now ${onOff}` });
- }) 
+
 // skriva http://localhost:3000/light/(lampas id)/on för att tända specifika lampan( skriv specifik id)
 //skriva http://localhost:3000/light/(lampas id)/(valfri text) för att släcka specifika lampan( skriv specifik id)
  app.get('/light/:id/:on', (req, res) =>{
@@ -36,37 +20,17 @@ app.listen(3000, () => {
      update();
      res.send({ msg: `device ${id} is now ${onOff}` });
  }) 
-// skriva http://localhost:3000/vacuum/VAC1/on för att starta dammsugaren
-// skriva http://localhost:3000/vacuum/VAC1/(valfri text) för att stopa dammsugaren
- app.get('/vacuum/:id/:on', (req, res) =>{
-    let id= req.params.id;
 
-     let onOff= req.params.on === "on" ? true : false
-     db.get('devices')
-     .find({id: id})
-     .assign({on : onOff})
-     .value();
-    
-     update();
-     res.send({ msg: `device ${id} is now ${onOff}` });
- }) 
-// skriva http://localhost:3000/speaker/SPE1/on för att starta högtalaren
-// skriva http://localhost:3000/speaker/SPE1/(valfri text)  för att stänga av högtalaren
- app.get('/speaker/:id/:on', (req, res) =>{
-    let id= req.params.id;
 
-     let onOff= req.params.on === "on" ? true : false
-     db.get('devices')
-     .find({id: id})
-     .assign({on : onOff})
-     .value();
-    
-     update();
-     res.send({ msg: `device ${id} is now ${onOff}` });
- }) 
-// skriva http://localhost:3000/blind/BLI1/on för att rulla ner rullgardin
-// skriva http://localhost:3000/blind/BLI1/(valfri text) för att rulla upp rullgardin
- app.get('/blind/:id/:on', (req, res) =>{
+// skriva http://localhost:3000/BLI1/on för att rulla ner rullgardin
+// skriva http://localhost:3000/BLI1/(valfri text) för att rulla upp rullgardin
+// skriva http://localhost:3000/SPE1/on för att starta högtalaren
+// skriva http://localhost:3000/SPE1/(valfri text)  för att stänga av högtalaren
+// skriva http://localhost:3000/VAC1/on för att starta dammsugaren
+// skriva http://localhost:3000/VAC1/(valfri text) för att stopa dammsugaren
+// skriva http://localhost:3000/CAM1/on för att starta kameran
+// skriva http://localhost:3000/CAM1/(valfri text) för att stänga av kameran
+ app.get('/:id/:on', (req, res) =>{
     let id= req.params.id;
 
      let onOff= req.params.on === "on" ? true : false
